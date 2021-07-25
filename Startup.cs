bc6b69c855d5eb58c262c2ea4640c4ee145c6299
Nhat Quang Ta <nhatquangta2911@gmail.com>
2021-07-25 18:45:15 +0700

    Added healthcheck live and ready

--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Text.Json;
 using System.Threading.Tasks;
 using dotnet_5_rest_api_flash.Interfaces;
 using dotnet_5_rest_api_flash.Repositories;
 using dotnet_5_rest_api_flash.Settings;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -35,22 +39,26 @@ namespace dotnet_5_rest_api_flash
       {
          BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
          BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+         var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
 
          services.AddSingleton<IMongoClient>(ServiceProvider =>
          {
-            var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-            return new MongoClient(settings.ConnectionString);
+            return new MongoClient(mongoDbSettings.ConnectionString);
          });
+
          services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
 
          services.AddControllers(options =>
          {
             options.SuppressAsyncSuffixInActionNames = false;
          });
+
          services.AddSwaggerGen(c =>
          {
             c.SwaggerDoc("v1", new OpenApiInfo { Title = "dotnet_5_rest_api_flash", Version = "v1" });
          });
+
+         services.AddHealthChecks().AddMongoDb(mongoDbSettings.ConnectionString, name: "mongodb", timeout: TimeSpan.FromSeconds(3), tags: new[] { "ready" });
       }
 
       // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -72,6 +80,32 @@ namespace dotnet_5_rest_api_flash
          app.UseEndpoints(endpoints =>
          {
             endpoints.MapControllers();
+            endpoints.MapHealthChecks("/healthcheck/ready", new HealthCheckOptions
+            {
+               Predicate = (check) => check.Tags.Contains("ready"),
+               ResponseWriter = async (context, report) =>
+               {
+                  var result = JsonSerializer.Serialize(
+                     new
+                     {
+                        status = report.Status.ToString(),
+                        checks = report.Entries.Select(entry => new
+                        {
+                           name = entry.Key,
+                           status = entry.Value.Status.ToString(),
+                           exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                           duration = entry.Value.Duration.ToString()
+                        })
+                     }
+                  );
+                  context.Response.ContentType = MediaTypeNames.Application.Json;
+                  await context.Response.WriteAsync(result);
+               }
+            });
+            endpoints.MapHealthChecks("/healthcheck/live", new HealthCheckOptions
+            {
+               Predicate = (_) => false
+            });
          });
       }
    }
